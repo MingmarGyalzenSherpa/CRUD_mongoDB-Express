45ec7e0f97f81e5e1310d78678f476384f35c17d
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2023-08-05 15:55:35 +0545

    insert functionality done

--- controller/crudController.js
+++ controller/crudController.js
@@ -1,10 +1,22 @@
 const getAllStudent = async (req, res) => {
   const db = req.db;
-  const cursor = db.collection("student").find();
+  const cursor = db.collection("students").find();
   const result = await cursor.toArray();
   res.status(200).json(result);
 };
 
+const insertStudent = async (req, res) => {
+  const db = req.db;
+  const { name, age, grade, address } = req.body;
+  try {
+    db.collection("students").insertOne({ name, age, grade, address });
+    res.status(200).json({ message: "Successfully added!!" });
+  } catch (error) {
+    res.status(400).json({ message: "Failed to insert student" });
+  }
+};
+
 module.exports = {
   getAllStudent,
+  insertStudent,
 };
--- routes/crud.routes.js
+++ routes/crud.routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { getAllStudent } = require("../controller/crudController");
+const {
+  getAllStudent,
+  insertStudent,
+} = require("../controller/crudController");
 const router = express.Router();
 
 router.get("/getStudent", getAllStudent);
+router.post("/insert_student", insertStudent);
 
 module.exports = router;
